9f3f27b209b7b6a65f15d161e0cee3d633682361
Åsmund Brekke <Ozzyz@users.noreply.github.com>
2018-09-26 13:24:38 +0200

    Update docker_setup.sh

--- docker_setup.sh
+++ docker_setup.sh
@@ -2,8 +2,6 @@
 pip install --upgrade pip
 ### DATA DOWNLOAD ###
 pip install kaggle
-export KAGGLE_USERNAME=aasmundbrekke
-export KAGGLE_KEY=2e4cb56a1863ae4d984e72004f63a6cc
 # Uncomment the following lines if you do not have the dataset downloaded
 kaggle competitions download -c rsna-pneumonia-detection-challenge
 # unzipping takes a few minutes
